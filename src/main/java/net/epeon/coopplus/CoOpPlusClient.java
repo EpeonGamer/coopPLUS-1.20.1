b956387160cf8ee370f0b1a518d31dd29458d329
Epeon <60582251+EpeonGamer@users.noreply.github.com>
2024-06-28 11:13:14 +0200

    Initial commit

---
+++ src/main/java/net/epeon/coopplus/CoOpPlusClient.java
@@ -0,0 +1,10 @@
+package net.epeon.coopplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoOpPlusClient  implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/epeon/coopplus/CoOpPlusDataGenerator.java
@@ -0,0 +1,11 @@
+package net.epeon.coopplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoOpPlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
